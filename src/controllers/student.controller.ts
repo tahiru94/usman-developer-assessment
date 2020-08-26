c32d99aba57fe73481a27a04f3cdc472a5fe4bff
tahiru94 <tahiru1994@engineer.com>
2020-08-25 22:00:56 -0400

    Add logic for displaying open assessments

--- src/controllers/student.controller.ts
+++ src/controllers/student.controller.ts
@@ -33,6 +33,25 @@ class StudentController {
         ctx.body = upcomingAssessments;
     }
 
+    async displayOpenAssessments(ctx: koa.Context, next: koa.Next) {
+        const idFromUrl: any = ctx.url.match(/\d+/);
+        const studentId: number = parseInt(idFromUrl[0]);
+        const selectedStudent: any = await studentRepo.getLogsByStudent(studentId);
+
+        const studentAssessments: any[] = selectedStudent.assessments;
+
+        // Retrieve open assessments
+        const currentDate = new Date().toISOString();
+        const openAssessments: any[] = studentAssessments.filter(assessment => {
+            // Make sure assessment is open (between open_time and close_time)
+            return moment(currentDate).isAfter(assessment.open_time) && moment(currentDate).isBefore(assessment.close_time);
+        }).sort((first, second) => {
+            return moment(first.open_time).diff(second.open_time);
+        });
+
+        ctx.body = openAssessments;
+    }
+
 }
 export const studentController = new StudentController();
 
@@ -44,5 +63,6 @@ const router = new Router()
 
 router.get('/student/display/:studentid', studentController.display)
 router.get('/student/assessment/upcoming/:studentId', studentController.displayUpcomingAssessments)
+router.get('/student/assessment/open/:studentId', studentController.displayOpenAssessments);
 
 export const StudentRouters = router;
