4b7f01100dbe98f08a681635f9656e76186eaab3
tahiru94 <tahiru1994@engineer.com>
2020-08-25 20:44:09 -0400

    Add logic for displaying upcoming assignments
    * Add moment to compare and sort dates
    * Update dates in the student repo file to emulate "upcoming"

--- src/controllers/student.controller.ts
+++ src/controllers/student.controller.ts
@@ -1,7 +1,9 @@
 // Routes
 import * as koa from 'koa';
 import * as Router from 'koa-router';
+import * as moment from 'moment';
 import { studentRepo } from '../repos/student.repo';
+import { Assessment } from '../models';
 
 class StudentController {
     // constructor(){}
@@ -14,6 +16,24 @@ class StudentController {
         ctx.body = selectedStudent;
     }
 
+    async displayUpcomingAssessments(ctx: koa.Context, next: koa.Next) {
+        const idFromUrl: any = ctx.url.match(/\d+/);
+        const studentId: number = parseInt(idFromUrl[0]);
+        const selectedStudent: any = await studentRepo.getLogsByStudent(studentId);
+
+        const studentAssessments: any[] = selectedStudent.assessments;
+
+        // Retrieve upcoming assessments
+        const currentDate = new Date().toISOString();
+        const upcomingAssessments: any[] = studentAssessments.filter(assessment => {
+            return moment(assessment.open_time).isAfter(currentDate);
+        }).sort((first, second) => {
+            return moment(first.open_time).diff(second.open_time);
+        });
+        
+        ctx.body = upcomingAssessments;
+    }
+
 }
 export const studentController = new StudentController();
 
@@ -24,5 +44,6 @@ export const studentController = new StudentController();
 const router = new Router()
 
 router.get('/student/display/:studentid', studentController.display)
+router.get('/student/assessment/upcoming/:studentId', studentController.displayUpcomingAssessments)
 
 export const StudentRouters = router;
