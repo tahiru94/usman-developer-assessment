3beb96be15dacd6d3a79b8a64ebc4f6790812c75
tahiru94 <tahiru1994@engineer.com>
2020-08-26 22:34:31 -0400

    Update ID / name for new assessment

--- src/repos/student.repo.ts
+++ src/repos/student.repo.ts
@@ -122,8 +122,8 @@ const student1 = {
             updated_by: null
         },
         {
-            id: 6,
-            name: "Exam-AE",
+            id: 7,
+            name: "Exam-AF",
             description: "NCE-2020:Exam-AE",
             open_time: "2020-06-07T00:00:00.000Z",
             close_time: "2029-06-05T00:00:00.000Z",
